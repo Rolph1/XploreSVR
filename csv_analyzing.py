83f4223d440f171d5ac2d480085a6b4305941dad
Rolph1 <rudolf.yazbeck@epfl.ch>
2024-05-13 18:55:11 +0300

    update

--- csv_analyzing.py
+++ csv_analyzing.py
@@ -1,30 +1,131 @@
 import csv
 import numpy as np
+from scipy.sparse import csr_matrix
+from sklearn.decomposition import TruncatedSVD
+
+'''
+def preprocessing(X):
+    X_transformed = []
+    for i in X:
+        X_sparse = csr_matrix(i)
+        svd = TruncatedSVD(n_components=300)
+        X_transformed.append(svd.fit_transform(X_sparse))
+        print(np.shape(np.array(X_transformed)))
+    return X_transformed
+    '''
+from scipy.sparse import csr_matrix
+from sklearn.decomposition import TruncatedSVD
+from sklearn.preprocessing import MinMaxScaler
+import numpy as np
 import math
 
+def preprocessing(X):
+    # Convert the entire dataset to a sparse matrix first
+    X_sparse = csr_matrix(X)
+    
+    # Initialize Truncated SVD with 300 components
+    # Note: Ensure that n_components is less than min(X.shape)
+    svd = TruncatedSVD(n_components=min(5, np.shape(np.array(X))[1]-1)) 
+    
+    # Apply SVD on the sparse matrix
+    X_transformed = svd.fit_transform(X_sparse)
+        # Initialize the Min-Max scaler
+    scaler = MinMaxScaler()
+    # Scale the transformed data to range [0, 1]
+    X_scaled = scaler.fit_transform(X_transformed)
+    #min(300, np.shape(np.array(X))[1]-1)
+    # Print shape to confirm it's 2D
+    print(np.shape(X_scaled))
+    print(np.array(X_scaled))
+    return X_transformed
 
 def normalize_rgb(x):
     return int(x)/255
 
+def rgb_to_hsi(R, G, B):
+    """
+    Converts rgb values to hsi
+    H is in [0, 360]
+    S is in [0, 100]
+    I is in [0, 255]
+    """
+    r, g, b = R / 255.0, G / 255.0, B / 255.0
+    i = (r+g+b) / 3
+    min_value = min(r, g, b)
+    s = 1 - (min_value) / (i + 1e-6) if i != 0 else 0
+    num = 0.5 * ((r - g) + (r - b))
+    den = np.sqrt((r - g)**2 + (r - b) * (g - b))
+    h = np.arccos(num / (den + 1e-6)) if den != 0 else 0
+    if b > g:
+        h = 2*np.pi - h
+    H = h*180 / np.pi   #[0, 360]
+    S = s*100           #[0,100]
+    I = i*255           #[0,255]
+    return H, S, I
+
+def grayscale(r, g, b):
+    return 0.2989 * r + 0.5870 * g + 0.1140 * b
 
 def find_occurences(X):
-    # setting all occurences to 0 initially
-    Y = [0]*(26**3)
+    """
+    Creates a vector of length 1792, with the first 768 elements being the occurences of
+    colors in the RGB color space, the second 768 elements being the occurences in the HSI 
+    color space, and finally the last 256 colors are the occurencses in the grayscale spectrum.
+    """
+    # Create a vector for all occurrences: RGB (768), HSI (768), and Grayscale (256)
+    RGB_LENGTH = 768
+    HSI_LENGTH = 768
+    GRAYSCALE_LENGTH = 256
+
+    RGB_BIN_DIVIDE = 12
+    HSI_H_MAX = 360
+    HSI_S_MAX = 100
+    HSI_I_MAX = 255
+
+    HSI_H_DIVIDE = 4
+    HSI_S_DIVIDE = 6
+    HSI_I_DIVIDE = 32
+
+    Y = [0] * (RGB_LENGTH + HSI_LENGTH + GRAYSCALE_LENGTH)
+
     for r, g, b in X:
-        r_index = math.ceil(r//10)
-        g_index = math.ceil(g//10)
-        b_index = math.ceil(b//10)
-        Y[r_index * (26**2) + g_index * 26 + b_index] += 1
+        # Putting RGB Indices in the vector
+        r_index = int(r // (RGB_LENGTH / RGB_BIN_DIVIDE))
+        g_index = int(g // (RGB_LENGTH / RGB_BIN_DIVIDE))
+        b_index = int(b // (RGB_LENGTH / RGB_BIN_DIVIDE))
+        rgb_index = r_index * (12**2) + g_index * 12 + b_index
+        Y[rgb_index] += 1
+        
+        # HSI Indices
+        H, S, I = rgb_to_hsi(r, g, b)
+
+        # prevent overflow
+        h_index = int(H / (HSI_H_MAX/HSI_H_DIVIDE))  # Divides 360 by 4
+        s_index = int(S / (HSI_S_MAX/HSI_S_DIVIDE))  # Divides 100 by 6
+        i_index = int(I / (HSI_I_MAX/HSI_I_DIVIDE))  # Divides 100 by 32
+
+        if h_index > HSI_H_DIVIDE - 1:
+            h_index = HSI_H_DIVIDE - 1
+        if s_index > HSI_S_DIVIDE - 1:
+            s_index = HSI_S_DIVIDE - 1
+        if i_index > HSI_I_DIVIDE - 1:
+            i_index = HSI_H_DIVIDE - 1
+
+        hsi_index = 768 + h_index * (6 * 32) + s_index * 32 + i_index
+        if hsi_index >= (768 + 768) or hsi_index < 768:
+            raise ValueError(f"Calculated HSI index is out of bounds: {hsi_index}, H: {H}, S: {S}, I:{I}")
+
+        Y[hsi_index] += 1
+
+        # Grayscale Index
+        gray_value = int(grayscale(r, g, b))
+        gray_index = 1536 + math.ceil(gray_value)
+        Y[gray_index] += 1
+    
     return Y
 
 
 def calculate_features(A):
-    """
-
-    :param A: Array containing information
-    :return: arary of features that are of importance to train the model
-
-    """
     r_ = [i[0] for i in A]
     g_ = [i[1] for i in A if len(i) > 1]
     b_ = [i[2] for i in A if len(i) > 2]
@@ -35,9 +136,8 @@ def calculate_features(A):
     r_std = np.std(r_, axis=0)
     g_std = np.std(g_, axis=0)
     b_std = np.std(b_, axis=0)
-    percentiles = np.percentile(A, [25, 50, 75], axis=0)
-
-    return [r_mean, g_mean, b_mean, r_std, g_std, b_std, percentiles]
+    #percentiles = np.percentile(A, [25, 50, 75], axis=0)
+    return [r_mean, g_mean, b_mean, r_std, g_std, b_std]
 
 
 def read_rows(filename, rows, X, fun):
--- training_svr.py
+++ training_svr.py
@@ -1,18 +1,17 @@
 import numpy as np
-from sklearn import svm
 from joblib import dump
 import csv_analyzing
-
+from lssvr import LSSVR 
 file_name = "img_data"
-images_to_test = [22, 10, 5]
-img_training = [i for i in range(15) if i not in images_to_test]
+images_to_test = [5, 10, 19] #, 12, 11, 24
+img_training = [i for i in range(3) if i not in images_to_test]
 data_filename = "sample_data.csv"
 X = []
 Y = []
-
+model = LSSVR(kernel='rbf', gamma=0.01)
 # placing the Fe2O3 concentration per image in the vector Y
 # sample, siO2, Fe2O3, TiO2, Al2O3 = line[0:5]
-csv_analyzing.read_rows(data_filename, [2], Y, lambda x: x)
+csv_analyzing.read_rows(data_filename, [4], Y, lambda x: x)
 
 print("finished reading chemical components")
 # currently only loops through the first x images and trains the model, going to change this to take a list of images
@@ -21,34 +20,51 @@ A = []
 for i in img_training:
     csv_filename = file_name + str(i)
     csv_analyzing.read_rows(csv_filename, range(3), A, lambda x: x)
-    X.append(csv_analyzing.find_occurences([(A[i], A[i+1], A[i+2]) for i in range(0, len(A), 3)]))
+    split_A = [(A[i], A[i+1], A[i+2]) for i in range(0, len(A), 3)]
+    # X.append(csv_analyzing.calculate_features(split_A))
+    occ = csv_analyzing.find_occurences(split_A)
+    X.append(occ)
+    print(len(X))
+    print(len(occ))
+    
+    #print(np.shape(np.array(X)))
 print("finished reading RGB components")
 
 
-regression_model = svm.SVR(C=1.0, kernel='rbf')
+regression_model = LSSVR(kernel='rbf', gamma=0.01)
 
 # selecting only the first x elements of Y to train the model as the rest will be used for testing
 Y_cut = []
 for i in img_training:
     Y_cut.append(Y[i])
 print("lenths: " + str(len(X)) + " " + str(len(Y_cut)))
+print("Y is " + str(Y))
 # for arr in X:
 #     print(str(len(arr)))
 
+#X_processed = csv_analyzing.preprocessing(X)
+#print(np.shape(np.array(X_processed)))
 # training model and placing the trained model in a joblib file to not have to train it again later on
 regression_model.fit(X, Y_cut)
 dump(regression_model, 'svr_model.joblib')
 print("model successfully trained!")
-
+Z = []
+ZZ = []
 # testing data prediction
 for i in images_to_test:
-    Z = []
-    ZZ = []
     csv_filename = file_name + str(i)
     csv_analyzing.read_rows(csv_filename, range(3), Z, lambda x: x)
-    ZZ.append(csv_analyzing.find_occurences([(Z[i], Z[i+1], Z[i+2]) for i in range(0, len(Z), 3)]))
-    prediction = regression_model.predict(ZZ)
-    for s in prediction:
+    occ = csv_analyzing.find_occurences([(Z[i], Z[i+1], Z[i+2]) for i in range(0, len(Z), 3)])
+    ZZ.append(occ)
+    print(len(occ))
+    print(len(ZZ))
+    
+    # ZZ.append(csv_analyzing.calculate_features(Z))
+print("prediction result on test data " + str(np.shape(np.array(ZZ))))
+prediction = []
+for i in ZZ:
+     prediction.append(regression_model.predict(np.reshape(np.array(i), (-1, 1))))
+for i, s in enumerate(prediction):
         #print("image number " + str(s) + ":")
         print("predicted value: " + str(s))
         print("actual value: " + str(Y[i]))
